29a596327e2ffcebc78663a9c1b9bb979c6b20c4
Mehryar <gargoyle.junior@gmail.com>
2026-09-16 03:48:13 +0330

    Responsiveness bugfix

--- app/test-drive/page.tsx
+++ app/test-drive/page.tsx
@@ -3,7 +3,7 @@ import Line from "@/components/ui/line";
 
 const TestDrive = () => {
   return (
-    <div className="max-w-6xl mx-auto py-15">
+    <div className="max-w-6xl mx-auto py-15 px-5 xl:px-0">
       <div className="flex flex-col gap-5 pt-15">
         <div className="flex gap-1 items-center text-[#b8935f] text-sm">
           <Line needsHide={false} />
