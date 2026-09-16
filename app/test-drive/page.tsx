bcad8b7eabe791f3a6c63016bc7913a97e113c76
Mehryar <gargoyle.junior@gmail.com>
2026-09-16 03:44:36 +0330

    Added test-drive page

--- app/test-drive/page.tsx
+++ app/test-drive/page.tsx
@@ -1,5 +1,19 @@
+import { ContactForm } from "@/components/form/form";
+import Line from "@/components/ui/line";
+
 const TestDrive = () => {
-  return <div>تست درایو</div>;
+  return (
+    <div className="max-w-6xl mx-auto py-15">
+      <div className="flex flex-col gap-5 pt-15">
+        <div className="flex gap-1 items-center text-[#b8935f] text-sm">
+          <Line needsHide={false} />
+          رزرو تست‌درایو
+        </div>
+
+        <ContactForm />
+      </div>
+    </div>
+  );
 };
 
 export default TestDrive;
